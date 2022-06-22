9f88bf258efb360c55864be3692901c573258c80
Jerzy DROZDZ <drozdz.jerzy@gmail.com>
2022-06-22 12:26:00 +0200

    Create SSRSConfigurationModel.cs

---
+++ SSRSConfigurationModel.cs
@@ -0,0 +1,11 @@
+namespace Example;
+
+
+public class SSRSConfigurationModel
+{
+      public string Username { get; set; }
+      public string Password { get; set; }
+      public string Url { get; set; }
+      public string Domain { get; set; }
+      public string Catalog { get; set; }
+}
